a941a9d8e6f783bdf3380fa2a724d7bcfd1794d1
dan-serraf <55733346+dan-serraf@users.noreply.github.com>
2021-02-08 19:33:30 +0100

    Update nettoyage.py
    Adaptation de l'extraction des données pour l'algorithme tf-idf

--- src/nettoyage.py
+++ src/nettoyage.py
@@ -1,16 +1,30 @@
 import os
+import unicodedata
+import string
+ 
 
+def supprimeAccent(string):
+    return unicodedata.normalize('NFKD', string).encode('ASCII', 'ignore').decode("utf-8")
 
-def extractionDonner(chemin_racine) :
+def minusculeString(string):
+    return string.lower()
+
+def supprimePonctuation(string):
+    for ponctuation in string.punctuation:
+        string = string.replace(ponctuation, "")
+    return string
+
+def extractionDonnerSeries(chemin_sous_repertoire_racine) :
     """
-    Fonction qui va extraire les titres des différents réperteroires ( et sous répertoires) ainsi que les fichiers textes.
+    Fonction qui va extraire les titres des différents episodes des differentes saison d'une series.
     
-    param : chemin_racine (string) -> chemin vers le dossier racine qui contient l'ensemble des sous repertoires et fichier texte.
-    return : liste[string] -> liste de tous les titres des series et épisodes
+    param : chemin_sous_repertoire_racine (string) -> chemin vers l'un des sous repertoires du dossier racine qui contient 
+    un ensemble de sous repertoires et fichier texte qui correspond a une serie avec le titre de ces episodes.
+    return : liste[string] -> liste de tous les titres des épisodes d'une série
     """
     mot = []
    
-    for root, directories, files in os.walk(chemin_racine):  
+    for root, directories, files in os.walk(chemin_sous_repertoire_racine):  
 
         #Parcours l'ensemble des fichiers
         for file in files:
@@ -39,6 +53,9 @@ def normaliseDonner(string):
     param : string -> chaine de caractere a normaliser
     return : string -> chaine de caractere normaliser.
     """
+    string = minusculeString(string) #mettre tous les mots en minuscules
+    string = supprimePonctuation(string) #remplace accent 
+    string = supprimePonctuation(string) #supprime la ponctuation
     return string
 
 def filtreDonner(liste) :
@@ -51,6 +68,18 @@ def filtreDonner(liste) :
     """
     return liste
 
+def extractionSeries(chemin_racine):
+    """
+    Fonction qui va extraire les titres des différents séries.
+    
+    param : chemin_sous_repertoire_racine (string) -> chemin vers l'un des sous repertoires du dossier racine qui contient 
+    un ensemble de sous repertoires et fichier texte qui correspond a une serie avec le titre de ces episodes.
+    return : liste1[liste2[string]]  -> une liste1 ou chaque element de la liste1 correspond a une serie.
+    La serie est une liste2[string] ou chaque element de cette liste correspond a un titre d'un episode de cette serie.
+    """
 
-
-
+    liste = []
+    for serie in os.listdir() :
+        liste.append(extractionDonnerSeries(serie))
+    
+    return serie
